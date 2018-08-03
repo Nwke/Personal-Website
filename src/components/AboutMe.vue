baba29ccea905393ad5c73dcd336df980133d653
Denis <d.mlglego@mail.ru>
2018-08-03 22:19:56 +0300

    Add files via upload

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,20 +2,17 @@
     <div>
       <v-app>
         <h1 class="display-2 mx-auto mb-4" center>
-          Приветствую,читатель
+          Добро пожаловать на мой сайт
         </h1>
 
         <div class="subheading">
           <span class="mb-2">
-            Итак,я обычный парень,который решил вести свой публичный дневник.<br>
-            Отднюдь на этом сайте я буду делиться с вами своей жизнью:<br>
-            Выкладывать видео,писать,о чем думаю.Инсайды.<br>
-            <span class="under">Цель сего сайта</span>:
-            Я же хочу здесь отследить свой прогресс за длительное время.<br>
-          </span>
-          <span class="mb-5">
-            <span class="under">Причина возникновения подобного:</span>
-            Спонтанная мысль.<br>
+            Добро пожаловать на мой сайт.Я увлекаюсь математикой,информатикой и программированием. <br>
+            Хочу создавать цифровой контент.Сейчас я студент первого курса по программе "прикладная математика" в университете МИРЭА. <br>
+            В последние несколько лет я работал над многими независимыми проектами наряду с моим формальным образованием. <br>
+            Моей целью является стать сильным разработчиком,чтобы в дальнейшем менять мир к лучшему,создавать иннованции. <br>
+            У меня есть advance знания в JavaScript, middle в Python.
+            Есть опыт создания пользовательских веб-приложений с использованием JavaScript,html,css,firebase,vue.js (как,например,этот сайт) <br>
           </span>
 
           <v-divider class="mt-2 mb-2"></v-divider>
@@ -26,13 +23,35 @@
         </div>
 
         <h1 class="display-2 mx-auto mt-4" center>
-          Достижения
+          Образование
         </h1>
 
         <div class="subheading mt-4">
-          <span>Получал дополнительное образование в <a href="https://foxford.ru/" target="_blank"> foxford </a> (2015-2018).<br>
-          Учился в онлайн-университете <a href="https://netology.ru/" target="_blank">Netology.ru</a>
-          (13.9.2017 по 25.12.2017) по направлению программирования ("Курс JavaScript в браузере: создаем интерактивные веб-страницы") и защитил диплом.<br></span>
+          <span>Получал дополнительное образование в <a href="https://foxford.ru/" target="_blank"> foxford </a> (2015-2018): <br>
+            <ul>
+              <li>Курсы по математике</li>
+              <li>Курсы по по информатике</li>
+              <li>Курсы по программированию</li>
+              <li>Курсы по английскому яз.</li>
+            </ul>
+          Учился в онлайн-университете <a href="https://netology.ru/" target="_blank">Netology</a> :
+            <ul>
+              <li>Курс по JavaScript в браузере: создаем интерактивные веб-страницы (Диплом защитил)</li>
+            </ul>
+          <br></span>
+          <span class="title">Мои проекты:</span> <br>
+          <a class="ml-2" href="http://mcards.tk" target="_blank">Карточки для запоминания</a> <br/>
+          <a class="ml-2" href="http://fastcot.tk/Menu" target="_blank">Тренажер устного счета</a> <br>
+          <a class="ml-2" href="http://onlinemath.tk" target="_blank">Онлайн-тренажер устного счета</a> <br>
+          <a class="ml-2" href="http://netology.tk" target="_blank">Стена с карточками</a> <br>
+        </div>
+
+        <h1 class="display-2 mx-auto mt-4" center>
+          Проекты
+        </h1>
+
+        <div class="subheading mt-4">
+
           <span class="title">Мои проекты:</span> <br>
           <a class="ml-2" href="http://mcards.tk" target="_blank">Карточки для запоминания</a> <br/>
           <a class="ml-2" href="http://fastcot.tk/Menu" target="_blank">Тренажер устного счета</a> <br>
